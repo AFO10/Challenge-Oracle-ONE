ee46233f32ef8fb8db8e22019512f9ca224c3cc7
AFO10 <89848233+AFO10@users.noreply.github.com>
2023-08-25 18:27:01 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Encriptador de Texto - Alura ONE
+
+Este proyecto es parte integral de mi formación como desarrollador en el programa Oracle ONE en colaboración con Alura. El proyecto consiste en crear un encriptador de texto siguiendo los criterios establecidos en el desafío. A través de esta experiencia, estoy adquiriendo habilidades prácticas y aplicando conceptos clave en el desarrollo de software.
---
+++ script.js
@@ -0,0 +1,89 @@
+const paraEncriptar = document.querySelector(".paraEncriptar");
+const paraDesencriptar = document.querySelector(".paraDesencriptar");
+
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
+
+// Funcion para encriptar
+function encriptar(textoParaEncriptar) {
+  let arregloCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+  textoParaEncriptar = textoParaEncriptar.toLowerCase();
+
+  for (let i = 0; i < arregloCodigo.length; i++) {
+    if (textoParaEncriptar.includes(arregloCodigo[i][0])) {
+      textoParaEncriptar = textoParaEncriptar.replaceAll(arregloCodigo[i][0], arregloCodigo[i][1]);
+    }
+  }
+  return textoParaEncriptar;
+}
+
+
+//Funcion validar texto
+function validarTextoEncriptar(texto) {
+  var patron = /^[a-z\s]+$/;
+  return patron.test(texto);
+}
+
+// Funcion para el boton de encriptar
+document.getElementById('encriptar').onclick = botonEncriptar;
+function botonEncriptar() {
+  const textoEncriptar = paraEncriptar.value;
+
+  if (validarTextoEncriptar(textoEncriptar)){
+    const stringEncriptado = encriptar(paraEncriptar.value);
+    paraDesencriptar.value = stringEncriptado;
+    paraEncriptar.value = "";
+    paraDesencriptar.style.backgroundImage = "none";
+    let botonCopy = document.getElementById("copy");
+    botonCopy.style.display = "block";
+  } else{
+    alert("Texto invalido, solo se permiten letras minúsculas y sin acentos")
+    paraEncriptar.value = ""
+  }
+
+}
+
+// Funcion para desencriptar
+function desencriptar(textoEncriptado) {
+  let arregloCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+  textoEncriptado = textoEncriptado.toLowerCase();
+
+  for (let i = 0; i < arregloCodigo.length; i++) {
+    if (textoEncriptado.includes(arregloCodigo[i][1])) {
+      textoEncriptado = textoEncriptado.replaceAll(arregloCodigo[i][1], arregloCodigo[i][0]);
+    }
+  }
+  return textoEncriptado;
+}
+
+
+// Funcion para el boton de desencriptar
+document.getElementById("desencriptar").onclick = botonDesencriptar;
+function botonDesencriptar() {
+  const stringDesencriptado = desencriptar(paraEncriptar.value);
+  paraEncriptar.value = "";
+  paraDesencriptar.value = stringDesencriptado;
+  paraDesencriptar.style.backgroundImage = "none";
+  let botonCopy = document.getElementById("copy");
+  botonCopy.style.display = "block";
+}
+
+// Funcion para copiar al text area y al portapapeles
+document.getElementById("copy").onclick=copiar;
+function copiar() {
+  let texto = paraDesencriptar.value;
+  navigator.clipboard.writeText(texto)
+  paraDesencriptar.value = " ";
+  paraEncriptar.value = texto;
+
+  let botonCopy = document.getElementById("copy");
+  botonCopy.style.display = "none";
+
+  paraDesencriptar.style.backgroundImage = "";
+  alert("Copiado al portapapeles y al campo de entrada");
+
+}
